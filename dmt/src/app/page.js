d1dd09248f64dfbaf3875f789e3214e6fc5f31dd
HugoDesignNW <hduranfe@sfu.ca>
2024-05-03 16:16:37 -0700

    my first app commit

---
+++ dmt/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-blue-500 text-white p-4 rounded-lg shadow-lg">
+        This is a simple div using Tailwind CSS!
+      </div>
+    </main>
+  );
+}
